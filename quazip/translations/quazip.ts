f5e4b61b06d3c9ad5e020bae3c492c2013d681a1
hakim <hakim@Saturn.local>
2012-11-06 20:52:41 +0700

    QuaZip porting for Blackberry 10
    Quazip, Qt based zip/unzip library for Blackberry 10

---
+++ quazip/translations/quazip.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>QuaGzipFile</name>
+    <message>
+        <location filename="../src/quagzipfile.cpp" line="36"/>
+        <source>Opening gzip for both reading and writing is not supported</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/quagzipfile.cpp" line="44"/>
+        <source>You can open a gzip either for reading or for writing. Which is it?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/quagzipfile.cpp" line="50"/>
+        <source>Could not gzopen() file</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
